950b547c3a20cfe143f202611ceb96360227afc8
Kurt Roeckx <kurt@roeckx.be>
2016-03-06 20:56:50 +0100

    Use an array of strings for the messages.

--- messages.c
+++ messages.c
@@ -2,6 +2,38 @@
 #include <string.h>
 #include "checks.h"
 
+static const char *error_strings[] =
+{
+	"E: Error parsing certificate\n",
+	"E: Issuer without organization name\n",
+	"E: Issuer without country name\n",
+	"E: Subject without OrganizationName but with StreetAddress\n",
+	"E: Subject with OrganizationName but without StateOrProvince or LocalityName\n",
+	"E: Subject without OrganizationName but with StateOrProvince or LocalityName\n",
+	"E: Fails decoding the characterset\n",
+	"E: Contains a null character in the distinguished name\n",
+	"E: Distinguished name contains invalid DirectoryString type\n",
+	"E: The distinguished name contains non-printable control characters\n",
+	"E: Subject with OrganizationName but without country name\n",
+	"E: Domain validated certificate but with organization name or address\n",
+	"E: Identity validated certificate but without organization name or address\n",
+	"E: No policy extension\n",
+	"E: No Subject alternative name extension\n",
+	"E: Certificate not version 3\n",
+	"E: Error parsing URL\n"
+};
+
+static const char *warning_strings[] = {
+	"W: The distinguished name contains something that is not a PrintableString or UTF8String\n",
+	"W: The distinguished name makes use of an IA5String\n"
+};
+
+static const char *info_strings[] = {
+	"I: Subject has a deprecated CommonName\n",
+	"I: String not checked\n",
+	"I: CRL is not a URL\n"
+};
+
 /* 
  * Turn the error information into strings.
  * Returns a buffer that should be free()d
@@ -14,87 +46,36 @@ char *get_messages()
 	buffer = malloc(8192);
 	buffer[0] = '\0';
 
-	if (GetBit(errors, ERR_INVALID))
+	for (int i = 0; i <= ERR_INVALID_URL; i++)
 	{
-		strcat(buffer, "E: Error parsing certificate\n");
-	}
-	if (GetBit(errors, ERR_ISSUER_ORG_NAME))
-	{
-		strcat(buffer, "E: Issuer without organization name\n");
-	}
-	if (GetBit(errors, ERR_ISSUER_COUNTRY))
-	{
-		strcat(buffer, "E: Issuer without country name\n");
-	}
-	if (GetBit(errors, ERR_SUBJECT_ADDR))
-	{
-		strcat(buffer, "E: Subject without OrganizationName but with StreetAddress\n");
-	}
-	if (GetBit(errors, ERR_SUBJECT_ORG_NO_PLACE))
-	{
-		strcat(buffer, "E: Subject with OrganizationName but without StateOrProvince or LocalityName\n");
-	}
-	if (GetBit(errors, ERR_SUBJECT_NO_ORG_PLACE))
-	{
-		strcat(buffer, "E: Subject without OrganizationName but with StateOrProvince or LocalityName\n");
-	}
-	if (GetBit(errors, ERR_INVALID_ENCODING))
-	{
-		strcat(buffer, "E: Fails decoding the characterset\n");
-	}
-	if (GetBit(errors, ERR_STRING_WITH_NUL))
-	{
-		strcat(buffer, "E: Contains a null character in the distinguished name\n");
-	}
-	if (GetBit(errors, ERR_INVALID_TAG_TYPE))
-	{
-		strcat(buffer, "E: Distinguished name contains invalid DirectoryString type\n");
-	}
-	if (GetBit(errors, ERR_NON_PRINTABLE))
-	{
-		strcat(buffer, "E: The distinguished name contains non-printable control characters\n");
-	}
-	if (GetBit(errors, ERR_SUBJECT_COUNTRY))
-	{
-		strcat(buffer, "E: Subject with OrganizationName but without country name\n");
-	}
-	if (GetBit(errors, ERR_DOMAIN_WITH_ORG_OR_ADDRESS))
-	{
-		strcat(buffer, "E: Domain validated certificate but with organization name or address\n");
-	}
-	if (GetBit(errors, ERR_IDENTITY_WITHOUT_ORG_OR_ADDRESS))
-	{
-		strcat(buffer, "E: Identity validated certificate but without organization name or address\n");
-	}
-	if (GetBit(errors, ERR_NO_POLICY))
-	{
-		strcat(buffer, "E: No policy extension\n");
-	}
-	if (GetBit(errors, ERR_NO_SUBJECT_ALT_NAME))
-	{
-		strcat(buffer, "E: No Subject alternative name extension\n");
-	}
-	if (GetBit(errors, ERR_NOT_VERSION3))
-	{
-		strcat(buffer, "E: Certificate not version 3\n");
-	}
-	if (GetBit(errors, ERR_INVALID_URL))
-	{
-		strcat(buffer, "E: Error parsing URL\n");
+		if (GetBit(errors, i))
+		{
+			strcat(buffer, error_strings[i]);
+		}
 	}
 
-	if (GetBit(warnings, WARN_NON_PRINTABLE_STRING))
+	for (int i = 0; i <= WARN_IA5; i++)
 	{
-		strcat(buffer, "W: The distinguage name contains something that is not a PrintableString or UTF8String\n");
+		if (i == WARN_IA5)
+		{
+			continue;
+		}
+		if (GetBit(warnings, i))
+		{
+			strcat(buffer, warning_strings[i]);
+		}
 	}
 
-	if (GetBit(info, INF_SUBJECT_CN))
+	for (int i = 0; i <= INF_CRL_NOT_URL; i++)
 	{
-		strcat(buffer, "I: Subject has a deprecated CommonName\n");
-	}
-	if (GetBit(info, INF_CRL_NOT_URL))
-	{
-		strcat(buffer, "I: CRL is not a URL\n");
+		if (i == INF_STRING_NOT_CHECKED)
+		{
+			continue;
+		}
+		if (GetBit(info, i))
+		{
+			strcat(buffer, info_strings[i]);
+		}
 	}
 
 	return buffer;
